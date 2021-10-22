de3b0edabb8130f58a7caf64e0ad6e6cf422b6c9
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-22 14:12:10 -0400

    21.10-dev-36

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ class searchAPI extends API {
 	public function search($request = null, $data = null){
 		if($data != null){
 			if(!is_array($data)){ $data = json_decode($data, true); }
-			$results = [
+			return = [
 				"success" => $this->Language->Field["Search query executed"],
 				"request" => $request,
 				"data" => $data,
