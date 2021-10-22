e4f3efe083449f61ea9e9200355b32cf0eb9e140
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-22 14:33:26 -0400

    21.10-dev-38

--- api.php
+++ api.php
@@ -4,11 +4,17 @@ class searchAPI extends API {
 	public function search($request = null, $data = null){
 		if($data != null){
 			if(!is_array($data)){ $data = json_decode($data, true); }
-			return [
-				"success" => $this->Language->Field["Search query executed"],
-				"request" => $request,
-				"data" => $data,
-			];
+			$results['request'] = $request;
+			$results['data'] = $data;
+			foreach($this->Settings['plugins'] as $plugin => $conf){
+				$api = $plugin.'API';
+				if(class_exists($api) && method_exists($api,'search')){
+					$results['output'][$plugin] = $api->search();
+				}
+			}
+			if(isset($results['output'])&&!empty($results['output'])){ $results['success'] = $this->Language->Field["Search query executed"]; }
+			else { $results['error'] = $this->Language->Field["No results found"]; }
+			return $results;
 		}
 	}
 }
